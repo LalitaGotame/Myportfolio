7a99d27d8e5eae8e4e909e0ac69d20e532582a82
Lalita Gotame <lalitagotame3@gmail.com>
2026-05-13 23:21:11 +0545

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./style.css";
-import ProjectCard from "./components/projectcard";
+import ProjectCard from "./components/ProjectCard";
 
 export default function App() {
   
