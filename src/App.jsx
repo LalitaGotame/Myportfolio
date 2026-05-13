35cfdb99660c521f5d7a1dd0137312044fb2a97b
Lalita Gotame <lalitagotame3@gmail.com>
2026-05-13 23:10:20 +0545

    fixed duplicate imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
 import About from "./components/About";
-import projectcard from "./components/projectcard"; 
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./style.css";
